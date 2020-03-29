5886e55a45f25df2f6c31ae1257d6d95023324fd
Warren Fischbeck <warren.fischbeck@gmail.com>
2020-03-29 11:03:38 -0500

    a

--- FischbeckEnterprises.FightClub.CharacterSheet/FightClubConverter/Converter_Equipment_Info.cs
+++ FischbeckEnterprises.FightClub.CharacterSheet/FightClubConverter/Converter_Equipment_Info.cs
@@ -11,20 +11,334 @@ namespace FischbeckEnterprises.FightClub.CharacterSheet.FightClubConverter
         private void Equipment()
         {
             string equipment = string.Empty;
+            string equipment2 = string.Empty;
             List<Item> equipmentItems = _pc.character.Where(x => x.item != null).FirstOrDefault().item.ToList();
             foreach(Item item in equipmentItems)
             {
-                if (string.IsNullOrEmpty(equipment))
+                switch (item.type)
                 {
-                    equipment = item.name;
-                }
-                else
-                {
-                    equipment += $", {item.name}";
+                    case 0:
+                        {
+                            if (string.IsNullOrEmpty(equipment))
+                            {
+                                equipment = item.name;
+                            }
+                            else
+                            {
+                                equipment += $", {item.name}";
+                            }
+                            if (item.quantity > 0)
+                            {
+                                equipment += $" - ({item.quantity})";
+                            }
+                            break; }
+                    case 1:
+                        {
+                            if (string.IsNullOrEmpty(equipment))
+                            {
+                                equipment = item.name;
+                            }
+                            else
+                            {
+                                equipment += $", {item.name}";
+                            }
+                            if (item.quantity > 0)
+                            {
+                                equipment += $" - ({item.quantity})";
+                            }
+                            break; }
+                    case 2:
+                        {
+                            if (string.IsNullOrEmpty(equipment))
+                            {
+                                equipment = item.name;
+                            }
+                            else
+                            {
+                                equipment += $", {item.name}";
+                            }
+                            if (item.quantity > 0)
+                            {
+                                equipment += $" - ({item.quantity})";
+                            }
+                            break; }
+                    case 3:
+                        {
+                            if (string.IsNullOrEmpty(equipment))
+                            {
+                                equipment = item.name;
+                            }
+                            else
+                            {
+                                equipment += $", {item.name}";
+                            }
+                            if (item.quantity > 0)
+                            {
+                                equipment += $" - ({item.quantity})";
+                            }
+                            break; }
+                    case 4:
+                        {
+                            if (string.IsNullOrEmpty(equipment))
+                            {
+                                equipment = item.name;
+                            }
+                            else
+                            {
+                                equipment += $", {item.name}";
+                            }
+                            if (item.quantity > 0)
+                            {
+                                equipment += $" - ({item.quantity})";
+                            }
+                            break; }
+                    case 5:
+                        {
+                            if (string.IsNullOrEmpty(equipment))
+                            {
+                                equipment = item.name;
+                            }
+                            else
+                            {
+                                equipment += $", {item.name}";
+                            }
+                            if (item.quantity > 0)
+                            {
+                                equipment += $" - ({item.quantity})";
+                            }
+                            break; }
+                    case 6:
+                        {
+                            if (string.IsNullOrEmpty(equipment))
+                            {
+                                equipment = item.name;
+                            }
+                            else
+                            {
+                                equipment += $", {item.name}";
+                            }
+                            if (item.quantity > 0)
+                            {
+                                equipment += $" - ({item.quantity})";
+                            }
+                            break; }
+                    case 7:
+                        {
+                            if (string.IsNullOrEmpty(equipment))
+                            {
+                                equipment = item.name;
+                            }
+                            else
+                            {
+                                equipment += $", {item.name}";
+                            }
+                            if (item.quantity > 0)
+                            {
+                                equipment += $" - ({item.quantity})";
+                            }
+                            break; }
+                    case 8:
+                        {
+                            if (string.IsNullOrEmpty(equipment2))
+                            {
+                                equipment2 = item.name;
+                            }
+                            else
+                            {
+                                equipment2 += $", {item.name}";
+                            }
+                            if (item.quantity > 0)
+                            {
+                                equipment2 += $" - ({item.quantity})";
+                            }
+                            break; }
+                    case 9:
+                        {
+                            if (string.IsNullOrEmpty(equipment2))
+                            {
+                                equipment2 = item.name;
+                            }
+                            else
+                            {
+                                equipment2 += $", {item.name}";
+                            }
+                            if (item.quantity > 0)
+                            {
+                                equipment2 += $" - ({item.quantity})";
+                            }
+                            break; }
+                    case 10:
+                        {
+                            if (string.IsNullOrEmpty(equipment))
+                            {
+                                equipment = item.name;
+                            }
+                            else
+                            {
+                                equipment += $", {item.name}";
+                            }
+                            if (item.quantity > 0)
+                            {
+                                equipment += $" - ({item.quantity})";
+                            }
+                            break; }
+                    case 11:
+                        {
+                            if (string.IsNullOrEmpty(equipment))
+                            {
+                                equipment = item.name;
+                            }
+                            else
+                            {
+                                equipment += $", {item.name}";
+                            }
+                            if (item.quantity > 0)
+                            {
+                                equipment += $" - ({item.quantity})";
+                            }
+                            break; }
+                    case 12:
+                        {
+                            if (string.IsNullOrEmpty(equipment))
+                            {
+                                equipment = item.name;
+                            }
+                            else
+                            {
+                                equipment += $", {item.name}";
+                            }
+                            if (item.quantity > 0)
+                            {
+                                equipment += $" - ({item.quantity})";
+                            }
+                            break; }
+                    case 13:
+                        {
+                            if (string.IsNullOrEmpty(equipment))
+                            {
+                                equipment = item.name;
+                            }
+                            else
+                            {
+                                equipment += $", {item.name}";
+                            }
+                            if (item.quantity > 0)
+                            {
+                                equipment += $" - ({item.quantity})";
+                            }
+                            break; }
+                    case 14:
+                        {
+                            if (string.IsNullOrEmpty(equipment))
+                            {
+                                equipment = item.name;
+                            }
+                            else
+                            {
+                                equipment += $", {item.name}";
+                            }
+                            if (item.quantity > 0)
+                            {
+                                equipment += $" - ({item.quantity})";
+                            }
+                            break; }
+                    case 15:
+                        {
+                            if (string.IsNullOrEmpty(equipment2))
+                            {
+                                equipment2 = item.name;
+                            }
+                            else
+                            {
+                                equipment2 += $", {item.name}";
+                            }
+                            if (item.quantity > 0)
+                            {
+                                equipment2 += $" - ({item.quantity})";
+                            }
+                            break;
+                        }
+                    case 16:
+                        {
+                            if (string.IsNullOrEmpty(equipment))
+                            {
+                                equipment = item.name;
+                            }
+                            else
+                            {
+                                equipment += $", {item.name}";
+                            }
+                            if (item.quantity > 0)
+                            {
+                                equipment += $" - ({item.quantity})";
+                            }
+                            break; }
+                    case 17:
+                        {
+                            if (string.IsNullOrEmpty(equipment))
+                            {
+                                equipment = item.name;
+                            }
+                            else
+                            {
+                                equipment += $", {item.name}";
+                            }
+                            if (item.quantity > 0)
+                            {
+                                equipment += $" - ({item.quantity})";
+                            }
+                            break; }
+                    case 18:
+                        {
+                            if (string.IsNullOrEmpty(equipment))
+                            {
+                                equipment = item.name;
+                            }
+                            else
+                            {
+                                equipment += $", {item.name}";
+                            }
+                            if (item.quantity > 0)
+                            {
+                                equipment += $" - ({item.quantity})";
+                            }
+                            break; }
+                    case 19:
+                        {
+                            if (string.IsNullOrEmpty(equipment))
+                            {
+                                equipment = item.name;
+                            }
+                            else
+                            {
+                                equipment += $", {item.name}";
+                            }
+                            if (item.quantity > 0)
+                            {
+                                equipment += $" - ({item.quantity})";
+                            }
+                            break; }
+                    case 20:
+                        {
+                            if (string.IsNullOrEmpty(equipment))
+                            {
+                                equipment = item.name;
+                            }
+                            else
+                            {
+                                equipment += $", {item.name}";
+                            }
+                            if (item.quantity > 0)
+                            {
+                                equipment += $" - ({item.quantity})";
+                            }
+                            break; }
+                    default:
+                        break;
                 }
             }
-
             _printablePlayerCharacter.Equipment1 = equipment;
+            _printablePlayerCharacter.Equipment2 = equipment2;
         }
     }
 }
