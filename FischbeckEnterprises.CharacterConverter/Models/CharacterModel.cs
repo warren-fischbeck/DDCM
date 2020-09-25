24afbc850d3208ea308fdb3fb9df9f8107553fd3
Warren Fischbeck <warren.fischbeck@gmail.com>
2020-09-25 13:15:56 -0500

    Starting a new project

---
+++ FischbeckEnterprises.CharacterConverter/Models/CharacterModel.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.NetworkInformation;
+using System.Text;
+
+namespace FischbeckEnterprises.CharacterConverter.Models
+{
+    public class CharacterModel
+    {
+        /// <summary>
+        /// charactername is the character name
+        /// </summary>
+        public string CharacterName { get; set; }
+        
+        /// <summary>
+        /// playername is the character owner name
+        /// </summary>
+        public string PlayerName { get; set; }
+        
+        /// <summary>
+        /// int value of the strength abilitiy score
+        /// </summary>
+        public int Strength { get; set; }
+        
+        /// <summary>
+        /// int value of the strength modifier
+        /// </summary>
+        public int StrenghtModifier { get { return ConvertScoreIntoModifier(this.Strength); } }
+        
+        /// <summary>
+        /// int value of the dexterity ability score
+        /// </summary>
+        public int Dexterity { get; set; }
+        
+        /// <summary>
+        /// int value of the dexterity modifier
+        /// </summary>
+        public int DexterityModifier { get { return ConvertScoreIntoModifier(this.Dexterity); } }
+       
+        /// <summary>
+        /// int value of the consitution ability score
+        /// </summary>
+        public int Constitution { get; set; }
+       
+        /// <summary>
+        /// int value of the consitution modifier
+        /// </summary>
+        public int ConstitutionModifier { get { return ConvertScoreIntoModifier(this.Constitution); } }
+        
+        /// <summary>
+        /// int value of the intelligence ability score
+        /// </summary>
+        public int Intellegence { get; set; }
+        
+        /// <summary>
+        /// int value of the intelligence modifier
+        /// </summary>
+        public int IntellegenceModifier { get { return ConvertScoreIntoModifier(this.Intellegence); } }
+        
+        /// <summary>
+        /// int value of the wisdom ability score
+        /// </summary>
+        public int Wisdom { get; set; }
+        
+        /// <summary>
+        /// int value of the wisdom modifier
+        /// </summary>
+        public int WisdomModifier { get { return ConvertScoreIntoModifier(this.Wisdom); } }
+        
+        /// <summary>
+        /// int value of the charisma ability score
+        /// </summary>
+        public int Charisma { get; set; }
+        
+        /// <summary>
+        /// int value of the charisma modifier
+        /// </summary>
+        public int CharismaModifier { get { return ConvertScoreIntoModifier(this.Charisma); } }
+
+        /// <summary>
+        /// int value of the experience points the character has
+        /// </summary>
+        public int ExperiencePoints { get; set; }
+
+        /// <summary>
+        /// calculates the ability score modifier for the passed in ability score
+        /// </summary>
+        /// <param name="AbilityScore">ability score to calculate</param>
+        /// <returns>int value of the ability score modifier</returns>
+        private int ConvertScoreIntoModifier(int AbilityScore) { return (AbilityScore - 10) / 2; }
+    }
+}
---
+++ FischbeckEnterprises.CharacterConverterConsole/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using FischbeckEnterprises.CharacterConverter;
+
+namespace FischbeckEnterprises.CharacterConverterConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Any letter other than y exits.");
+            bool loop = true;
+            do
+            {
+                string r = Console.ReadLine();
+                if (r == "y")
+                {
+                    CharacterConverter.Models.CharacterModel characterModel = new CharacterConverter.Models.CharacterModel()
+                    {
+                        CharacterName = "Test Character",
+                        PlayerName = "Warren Fischbeck",
+                        Charisma = new RandomAbility().AbilityScroe,
+                        Constitution = new RandomAbility().AbilityScroe,
+                        Dexterity = new RandomAbility().AbilityScroe,
+                        Intellegence = new RandomAbility().AbilityScroe,
+                        Strength = new RandomAbility().AbilityScroe,
+                        Wisdom = new RandomAbility().AbilityScroe
+                    };
+                    Console.WriteLine($"{characterModel.PlayerName}'s character: {characterModel.CharacterName}");
+                    Console.WriteLine($"Strenght:     {characterModel.Strength} - {characterModel.StrenghtModifier}");
+                    Console.WriteLine($"Dexterity:    {characterModel.Dexterity} - {characterModel.DexterityModifier}");
+                    Console.WriteLine($"Constitution: {characterModel.Constitution} - {characterModel.ConstitutionModifier}");
+                    Console.WriteLine($"Intellegence: {characterModel.Intellegence} - {characterModel.IntellegenceModifier}");
+                    Console.WriteLine($"Wisdom:       {characterModel.Wisdom} - {characterModel.WisdomModifier}");
+                    Console.WriteLine($"Charisma:     {characterModel.Charisma} - {characterModel.CharismaModifier}");
+                    Console.WriteLine("\nGo again?");
+                }
+                else { loop = false; }
+            } while (loop);
+        }
+    }
+
+    class RandomAbility
+    {
+        private Random random = new Random();
+        
+        
+        private List<int> sum;
+
+        private int sumResult;
+        public int AbilityScroe { get { return sumResult; } }
+
+        public RandomAbility()
+        {
+            sum = new List<int>();
+            for (int i = 0; i < 4; i++)
+            {
+                sum.Add(random.Next(1, 7));
+            }
+            sumResult = GenerateAbilityScore();
+        }
+
+        private int GenerateAbilityScore()
+        {
+            int lowest = sum.Min();
+            sum.Remove(lowest);
+            return sum.Sum();
+        }
+    }
+}
